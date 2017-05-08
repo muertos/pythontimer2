16ba29d61b62c12e1fdce5d3c421ee6d4b159e44
Nick West <mmmaaiilll@yahoo.com>
2017-05-08 14:02:24 -0400

    fucking works

--- threadtest.py
+++ threadtest.py
@@ -8,25 +8,41 @@ class Worker(threading.Thread):
     quit = False
     
     def run(self):
+        print "entered worker.run"
+        time.sleep(1)
         while not self.quit:
+            print self.timerIsRunning
             if self.timerIsRunning:
+                print "timer is running"
                 max_seconds = 10
                 count = 0
                 curr = sec_count = int(strftime("%S", gmtime()))
-                while count < max_seconds:
+                while self.timerIsRunning:
                     sec_count = int(strftime("%S", gmtime()))
                     if sec_count != curr:
                         count = count + 1
+                        if count > max_seconds:
+                            timerIsRunning = False
+                            break
                         curr = sec_count
                         print "."
+                    
             else:
-                print "timer not running" #we know this occurs, so the worker is spawned
-                time.sleep(0.1)
+                print "timer should not be running"
+                print self.timerIsRunning
+                time.sleep(1)
+
+#    def starttimer(self, widget):
+#        timerIsRunning = True
+
+#    def stoptimer(self, widget):
+#        timerIsRunning = False
 
 class Window(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="thread testing")
         self.worker = Worker() 
+        #self.worker.run()
         self.worker.start() #explore what this does
 
         def command(arg):
